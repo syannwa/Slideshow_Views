83a8230d6867ca22213e214de64b838f6b8f7318
Sydney Waters <saw465712@uga.edu>
2019-08-27 22:28:09 -0400

    Initial Commit

---
+++ Assignment_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by Sydney Ann Waters on 8/27/19.
+//  Copyright © 2019 Sydney Ann Waters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
